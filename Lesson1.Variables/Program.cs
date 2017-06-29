391d4d1583018c4d69b2ddebb43fe314a9246b83
ruslan878707@gmail.com <19871961>
2017-06-29 17:17:06 +0300

    Add Lesson1.Variables.csproj

---
+++ Lesson1.Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lesson1.Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello world";
+            int num = 454;
+            bool boolean = true;
+
+
+            Console.WriteLine(str);
+            Console.WriteLine(num);
+            Console.WriteLine(boolean);
+
+            Console.ReadKey();
+        }
+    }
+}
